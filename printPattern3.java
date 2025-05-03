3631ca91b1160fc74417b11276cd7799408e2482
SrutiDash <srutidash1203@gmail.com>
2025-05-03 17:21:02 +0530

    updated

--- printPattern3.java
+++ printPattern3.java
@@ -3,7 +3,6 @@
 public class printPattern3 {
     public static void main(String[] args) {
         int n = 4;
-        int m = 4;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
