ff67f3cb4c467cc037725f4868855a7326140603
SrutiDash <srutidash1203@gmail.com>
2025-05-09 20:37:47 +0530

    patterns

--- patterns.java
+++ patterns.java
@@ -1,7 +1,10 @@
 public class patterns {
     public static void main(String[] args) {
-        for(int i=1;i<=4;i++){
-            System.out.println(i);
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){ //Do this 4 times for 4 rows.
+                System.out.print("* "); //Print 4 stars.
+            }
+            System.out.println(" ");  //Go to the next line.
         }
     }
 }
