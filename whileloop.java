0c6dcf43335274067f746ac679f68e4d5d80975d
SrutiDash <srutidash1203@gmail.com>
2025-05-11 22:02:30 +0530

    N natural number and sum of n natural numbers using while loop

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+
+        int j=1;
+        int sum=0;
+        while(j<=5){
+            sum+=j;
+            j++;
+        }
+        System.out.println("sum = "+sum);
+    }
+}
