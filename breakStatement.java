02b3a0a1c620b6439906710a25aab0bf9ad44ef0
SrutiDash <srutidash1203@gmail.com>
2025-05-14 11:45:37 +0530

    break statement

---
+++ breakStatement.java
@@ -0,0 +1,10 @@
+public class breakStatement{
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+            System.out.println(i);
+            if(i>=10){
+                break;
+            }
+        }
+    }
+}
