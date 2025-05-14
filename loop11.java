3c389d6db2e47f8fdf85a90020097e0ec75e1841
SrutiDash <srutidash1203@gmail.com>
2025-05-14 17:56:20 +0530

    commit changes

--- loop11.java
+++ loop11.java
@@ -3,11 +3,11 @@ public class loop11 {
         for(int i=0;i<=20;i++){
             if(i==3 || i==8){
                 continue;
-            }
+            } 
 
             if(i>=15){
                 break;
-            }
+            }  
             System.out.println(i);
         }
         
