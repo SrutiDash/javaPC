ec2fb982b8dc5ea1c1ed1328a5e19596a200310e
SrutiDash <srutidash1203@gmail.com>
2025-05-14 16:09:41 +0530

    break and continue

--- loop11.java
+++ loop11.java
@@ -1,5 +1,15 @@
 public class loop11 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        for(int i=0;i<=20;i++){
+            if(i==3 || i==8){
+                continue;
+            }
+
+            if(i>=15){
+                break;
+            }
+            System.out.println(i);
+        }
+        
     }
 }
