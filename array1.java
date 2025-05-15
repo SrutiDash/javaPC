41a0226e09563a365f86931780542dd86778f541
SrutiDash <srutidash1203@gmail.com>
2025-05-15 13:53:39 +0530

    declaration and initialisation

--- array1.java
+++ array1.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 
 public class array1 {
     public static void main(String[] args) {
-        int[] array;
-        array = new int[30]; 
+        int[] array; //declaration
+        array = new int[30]; //initialisation and memory allocation
         System.out.println(Arrays.toString(array));
     }
 }
