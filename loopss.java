6a65f25a62643d12e0d52db3097fd841200d8918
SrutiDash <srutidash1203@gmail.com>
2025-05-16 13:18:35 +0530

    infinite loop

---
+++ loopss.java
@@ -0,0 +1,9 @@
+public class loopss {
+    public static void main(String[] args) {
+        //example of infinite loop
+
+        while(true){
+            System.out.println("This is an infinite loop");
+        }
+    }
+}
