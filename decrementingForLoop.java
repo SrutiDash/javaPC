f4897e3de383d2cef910673c623713f778556771
SrutiDash <srutidash1203@gmail.com>
2025-05-17 20:57:11 +0530

    decrementing for loop

---
+++ decrementingForLoop.java
@@ -0,0 +1,8 @@
+public class decrementingForLoop {
+    public static void main(String[] args) {
+        System.out.println("sbus");
+        for(int i=7;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
