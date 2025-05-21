220f6ecdbbb840f4e429b0dfe85056fa01984f5a
SrutiDash <srutidash1203@gmail.com>
2025-05-21 08:44:24 +0530

    sum of table 8

---
+++ sumof8Table.java
@@ -0,0 +1,10 @@
+public class sumof8Table {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            int n = 8;
+            sum+=8*i;
+        }
+        System.out.println(sum);
+    }
+}
