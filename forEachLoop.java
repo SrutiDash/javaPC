2ef2223bcf06028d36a58e277c41e9d7430fc7de
SrutiDash <srutidash1203@gmail.com>
2025-05-21 12:43:31 +0530

    display the elements of an array using for each loop

---
+++ forEachLoop.java
@@ -0,0 +1,13 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        //for each loop :
+        int arr[] = {34,56,6,7,78,87};
+
+        //display the elements of an array using for each loop
+
+        //simplest method tho : 
+        for(int element : arr){
+            System.out.println(element);
+        }
+    }
+}
