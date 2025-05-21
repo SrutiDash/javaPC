82c874f7877afd0201a9fcb702e864a8225273f5
SrutiDash <srutidash1203@gmail.com>
2025-05-21 07:55:30 +0530

    substring and replace method in string

--- string.java
+++ string.java
@@ -7,5 +7,12 @@ public class string{
 
         String name2 = new String("sruti"); //using new keyword and string constructor
         System.out.println(name2);
+
+        System.out.println(name.substring(2)); //returns from 2nd to nth index
+        System.out.println(name.substring(1, 4)); //returns from 1st index to (endindex-1)
+
+        System.out.println(name.replace('s', 'p')); //replaces old char to new char in string
+        System.out.println(name.replace("s", "pa")); //replaces string from target to replacement
+        
     }
 }
