3c30a07de9a57448bf8c800b37d0f8c2bd684988
SrutiDash <srutidash1203@gmail.com>
2025-05-21 11:20:41 +0530

    array

---
+++ arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        array[0] = 45;
+        array[1] = 46;
+        array[2] = 47;
+        array[3] = 48;
+        array[4] = 49;
+        System.out.println(array[3]);
+        array[3] = 66;  //changing it's value
+        System.out.println(array[3]);
+    }
+}
+
