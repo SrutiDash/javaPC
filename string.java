9f7b680c29ec20c87e73b7f5726749dece2a0595
SrutiDash <srutidash1203@gmail.com>
2025-05-21 07:34:30 +0530

    string literal and declaring it using new keyword which is basically creating an object

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string{
+    public static void main(String[] args) {
+        String name = "sruti";  //using string literal
+        System.out.println(name);
+
+        //but we can also write string in this way :
+
+        String name2 = new String("sruti"); //using new keyword and string constructor
+        System.out.println(name2);
+    }
+}
