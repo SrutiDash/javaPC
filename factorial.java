ca5080dac7a9e6ad3736995ca3e5464a0c2c9b5d
SrutiDash <srutidash1203@gmail.com>
2025-05-21 08:22:25 +0530

    factorial of a number

---
+++ factorial.java
@@ -0,0 +1,9 @@
+public class factorial {
+    public static void main(String[] args) {
+        int n=1;
+        for(int i=6;i>=1;i--){
+            n*=i;
+        }
+        System.out.println(n);
+    }
+}
