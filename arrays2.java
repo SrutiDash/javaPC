984d630e1c6a6dd2b2bfb57b8ba511d473e9d1f7
SrutiDash <srutidash1203@gmail.com>
2025-05-21 12:24:18 +0530

    array.length for length of an array and printing the array's index values using for loop and array.length

---
+++ arrays2.java
@@ -0,0 +1,14 @@
+public class arrays2 {
+    public static void main(String[] args) {
+        int arr[] = {23, 67, 87, 24, 43};
+        System.out.println(arr.length);
+        String[] array = {"sruti", "elyy", "netflix", "amazon"};
+        System.out.println(array[3]);
+        System.out.println(array.length);
+        //if we want to display values in each index :
+
+        for(int i=1;i<=array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
