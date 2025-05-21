361110baf934b68d17c0c54dd44c25f796d46c93
SrutiDash <srutidash1203@gmail.com>
2025-05-21 12:07:00 +0530

    three ways to create an array

--- arrays.java
+++ arrays.java
@@ -1,6 +1,15 @@
 public class arrays {
     public static void main(String[] args) {
+        //1
+        //initialization
+        int[] marks;
+        //memory allocation
+        marks = new int[5];
+        
+        //2
+        //intitialization and memory allocation
         int[] array = new int[5];
+        //initialization
         array[0] = 45;
         array[1] = 46;
         array[2] = 47;
@@ -8,7 +17,11 @@ public class arrays {
         array[4] = 49;
         System.out.println(array[3]);
         array[3] = 66;  //changing it's value
+
+        //3
         System.out.println(array[3]);
+        //initialization memory allocation(java do automatically) and initialization
+        int arr[] = {23, 67, 87, 24, 43};
     }
 }
 
