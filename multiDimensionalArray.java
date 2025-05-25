85464f7f6f8c96400ac3bf0b062135f373f86094
SrutiDash <srutidash1203@gmail.com>
2025-05-25 18:40:21 +0530

    commit

--- multiDimensionalArray.java
+++ multiDimensionalArray.java
@@ -11,5 +11,6 @@ public class multiDimensionalArray {
         System.out.println(Arrays.deepToString(marks));
         System.out.println(Arrays.toString(marks[0]));
         System.out.println("sruti dash");
+        System.out.println("sruti dash");
     }
 }
